20d995996785ed19f8d12a7020008de89f11a6f4
Iia Haavanoksa <Iia.Haavanoksa@ehyt.fi>
2022-10-08 18:29:27 +0300

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet,View, TextInput, Button } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+
+  const [text, setText] = useState("");
+
+  const speak = () => {
+    Speech.speak(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text} />
+        <View style={{ flexDirection: 'row', marginTop: 10}}>
+          <View style={{ marginRight: 10}}>
+            <Button onPress={speak} title="Press to hear text" />
+          </View>
+      </View>
     </View>
   );
 }
@@ -16,5 +28,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: '20%'
   },
+  input: {
+    width:200  , 
+    borderColor: 'gray', 
+    borderWidth: 1,
+    margin: 5
+  }
 });
